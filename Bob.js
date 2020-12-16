65f916ef2f232ededd92e3d9fc1ba99e83c2159b
Divyanshu-Kashyap <73643943+Divyanshu-Kashyap@users.noreply.github.com>
2020-12-16 12:52:15 +0530

    Add files via upload

---
+++ Bob.js
@@ -0,0 +1,27 @@
+class Bob{
+    constructor(x,y){
+        var options={
+            isStatic:false,
+            restitution:1.2,
+            friction:0,
+            density:7,
+        }
+
+        this.body=Bodies.circle(x,y,25,options);
+        World.add(world,this.body);
+    }
+
+   display(){
+       push();
+       ellipseMode(RADIUS);
+       strokeWeight(2);
+       stroke("purple");
+       fill("black");
+       ellipse(this.body.position.x,this.body.position.y,25,25);
+       pop();
+   }
+
+
+
+
+}
